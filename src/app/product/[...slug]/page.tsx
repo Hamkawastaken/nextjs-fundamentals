0448c8c66576ac7225c8b996b315c86fd2c5ea67
Muhammad Hamka <hamkarifai49@gmail.com>
2025-06-23 09:32:04 +0700

    dynamic routing

---
+++ src/app/product/[...slug]/page.tsx
@@ -0,0 +1,18 @@
+type DetailProductPageProps = { params: { slug: string[] } };
+
+export default function DetailProductPage(props: DetailProductPageProps) {
+  const { params } = props;
+  console.log(params);
+  return (
+    <>
+      <h1>Detail Product Page</h1>
+      {params.slug && (
+        <>
+          <p>Category : {params.slug[0]}</p>
+          <p>Gender : {params.slug[1]}</p>
+          <p>Brand : {params.slug[2]}</p>
+        </>
+      )}
+    </>
+  );
+}
